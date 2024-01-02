62a8a8ecd6308fb02794293bbbc18262ff32e232
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-01-02 15:39:18 -0300

    free access to ngrok domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins '*.ngrok-free.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
